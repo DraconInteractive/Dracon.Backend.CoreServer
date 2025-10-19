049e568938576677b951f1014890d8c7a389a36d
Peter Carey <petermarkcarey@gmail.com>
2025-10-19 17:44:04 +0800

    ci: publish to azure

--- obj/Debug/net9.0/CoreServer.AssemblyInfo.cs
+++ obj/Debug/net9.0/CoreServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc9bdea662fee3d9f5a3f59921c275b1c573638")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc6338ebde98b3ce52ce42d830f582d7754ae5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
