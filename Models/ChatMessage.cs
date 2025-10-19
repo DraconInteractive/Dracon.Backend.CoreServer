94db23cddc2eaf81eadb4c13178747a0f08e6609
Peter Carey <petermarkcarey@gmail.com>
2025-10-19 18:00:15 +0800

    fix: forgot to commit chat message model structure

---
+++ Models/ChatMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CoreServer.Models;
+
+public class ChatMessage
+{
+    public string sender { get; set; } = string.Empty;
+    public string id { get; set; } = string.Empty;
+    public string text { get; set; } = string.Empty;
+    public DateTimeOffset ts { get; set; }
+}
